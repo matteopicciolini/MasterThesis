db4d5c28c3e83e3436a269f97b149d38e598b34b
Matteo Picciolini <picciolinimatteo@gmail.com>
2024-10-27 12:43:32 +0100

    shear

--- reader_8.py
+++ reader_8.py
@@ -4,6 +4,7 @@ from halo import HaloCatalogManager
 
 # Parameters for maps and catalogs
 n_side = 4096
+l_max = 3 * n_side - 1
 n_pix = hp.nside2npix(n_side)
 
 # Select halo catalogs with z < 0.5 and initialize the manager
@@ -15,3 +16,9 @@ mass_threshold = 1e2
 filtered_manager = manager.select_mass_above(mass_threshold)
 
 halos = filtered_manager.get_all_halos()
+
+# Spherical transform of the convergence map
+alm_kappa = hp.map2alm(halos.convergence_halos, lmax=l_max)
+
+# Convert the alm of the convergence to a spin-2 shear field
+shear_map = hp.alm2map_spin([alm_kappa, alm_kappa], nside=4096, spin=2, lmax=l_max)
